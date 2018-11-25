7589ec58d14312340a3351375bd44c1dc648d977
greenlinux <yinyanjun@gmail.com>
2018-11-26 06:20:40 +0800

    change to maven project in Intellj IDEA

---
+++ src/main/java/cn/greenlinux/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.greenlinux.helloworld;
+
+/**
+ * 哈喽，世界！
+ * <p>
+ * 我是Greenlinux，这里是我的代码空间。
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World, yinyj!");
+    }
+}
